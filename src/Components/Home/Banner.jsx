2f8309d5166446668f3a019c1c959b8327dfa416
MamtazDev <akternipa941@gmail.com>
2023-12-07 15:57:13 +0600

    update

---
+++ src/Components/Home/Banner.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Auth from "../../Shared/Auth";
+
+const Banner = () => {
+  return <Auth>
+    dxhfvjhfbvjbr
+    jhvjcbvnc
+    brjsdcjb
+  </Auth>;
+};
+
+export default Banner;
