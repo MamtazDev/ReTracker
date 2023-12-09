8d7bd6eb6196291f2db48ad7b828bfa57b041bfc
MamtazDev <akternipa941@gmail.com>
2023-12-09 10:56:48 +0600

    update login checkbox

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -2,8 +2,14 @@ import PrimaryBtn from "../../Shared/PrimaryBtn";
 import AuthTitle from "../../Shared/AuthTitle";
 import Divider from "../../Shared/Divider";
 import SocialBtn from "../../Shared/SocialBtn";
+import { useState } from "react";
 
 const Login = () => {
+  const [isChecked, setChecked] = useState(false);
+
+  const handleCheckboxChange = () => {
+    setChecked(!isChecked);
+  };
   return (
     <div className="pb-28 pt-p_153 max-w-authWidth mx-auto">
       <AuthTitle>Login to Your Account</AuthTitle>
@@ -25,16 +31,28 @@ const Login = () => {
         </div>
 
         <div className="flex justify-between items-center gap-3 mb-10">
-          <div className="flex gap-3 items-center">
-            <input type="checkbox" id="stay" />
-            <label className="mb-0" htmlFor="stay">
-              Stay signed in
-            </label>
-          </div>
+          <label className="flex items-center gap-4">
+            <input
+              className="hidden"
+              type="checkbox"
+              checked={isChecked}
+              onChange={handleCheckboxChange}
+            />
+            <span
+              className={`${
+                isChecked && "bg-slate-200"
+              } border  border-slate-200 rounded-sm h-[18px] w-[18px] flex items-center justify-center`}
+            >
+              {isChecked ? "✔" : ""}
+            </span>
+            Stay signed in
+          </label>
+
           <p className="text-primary font-semibold text-sm ">
             I forgot my password
           </p>
         </div>
+
         <PrimaryBtn>Login</PrimaryBtn>
       </form>
     </div>
