c103c55d8fb9eb6f4d7d9c012ba249724ba0348f
muhammadashiqurrahman <getintouch.ashiq@gmail.com>
2023-12-11 12:00:01 +0600

    banner size adujust

--- src/Shared/Header.jsx
+++ src/Shared/Header.jsx
@@ -5,7 +5,6 @@ import responsiveBtn from '../assets/res-btn.png';
 
 const Header = () => {
   let location = useLocation();
-
   const [isOpen, setIsOpen] = useState(false)
   const toggleResponsiveNav = () => {
     setIsOpen(!isOpen);
@@ -25,32 +24,32 @@ const Header = () => {
                 </Link>
               </div>
 
-              <div className="responsive_btn sm:hidden ml-auto">
+              <div className="responsive_btnmd:hidden ml-auto">
                 <button onClick={toggleResponsiveNav}>
                   <img src={responsiveBtn} className="cursor-pointer" alt="responsive_btn" />
                 </button>
               </div>
             </div>
           ) : (
-              <>
-                <div className={"nav-brand"}>
-                  <Link to={"/"} className="flex items-center gap-2 text">
-                    <img src={brandLogo} alt="brand-logo" />
-                  </Link>
-                </div>
+            <>
+              <div className={"nav-brand"}>
+                <Link to={"/"} className="flex items-center gap-2 text">
+                  <img src={brandLogo} alt="brand-logo" />
+                </Link>
+              </div>
 
-                <div className="responsive_btn sm:hidden ml-auto">
-                  <button onClick={toggleResponsiveNav}>
-                    <img src={responsiveBtn} className="cursor-pointer" alt="responsive_btn" />
-                  </button>
-                </div>
-              </>
+              <div className="responsive_btn md:hidden ml-auto">
+                <button onClick={toggleResponsiveNav}>
+                  <img src={responsiveBtn} className="cursor-pointer" alt="responsive_btn" />
+                </button>
+              </div>
+            </>
           )}
 
 
 
 
-          <div className={isOpen ? "block" : "navbar mx-auto hidden sm:block"}>
+          <div className={isOpen ? "block" : "navbar mx-auto hidden md:block"}>
             <ul className={isOpen ? "navbar-nav block" : "navbar-nav flex flex-wrap items-center gap-11"}>
               <li className={isOpen ? "nav-items mb-8" : "nav-items"}>
                 <Link
@@ -90,7 +89,7 @@ const Header = () => {
             </ul>
           </div>
 
-          <div className={isOpen ? "block" : "userBtn hidden sm:block"} >
+          <div className={isOpen ? "block" : "userBtn hidden md:block"} >
             <div className="flex flex-wrap gap-4 ">
               {location.pathname !==
                 ("/login" &&
