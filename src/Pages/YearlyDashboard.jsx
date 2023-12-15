e147b705cedb1e844bc2ba0401e1eff97b3c0508
MamtazDev <akternipa941@gmail.com>
2023-12-15 18:27:54 +0600

    category section add

--- src/Pages/YearlyDashboard.jsx
+++ src/Pages/YearlyDashboard.jsx
@@ -2,14 +2,16 @@ import React from "react";
 import CalendarHeader from "../Components/Calender/CalendarHeader";
 import BannerList from "../Components/YearlyDashboard/BannerList";
 import HourSpent from "../Components/YearlyDashboard/HourSpent";
+import Category from "../Components/YearlyDashboard/Category";
 
 const YearlyDashboard = () => {
   return (
     <div className="p-8">
       <CalendarHeader />
       <BannerList />
-      <div className="grid grid-cols-1 lg:grid-cols-2">
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
         <HourSpent />
+        <Category/>
       </div>
     </div>
   );
