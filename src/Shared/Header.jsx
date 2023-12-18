2a9b94c833181cfe768c7638d2a721c744255923
MamtazDev <akternipa941@gmail.com>
2023-12-18 10:57:01 +0600

    conflict resolve

--- src/Shared/Header.jsx
+++ src/Shared/Header.jsx
@@ -12,42 +12,51 @@ const Header = () => {
 
   return (
     <div className="mx-auto max-w-[280px] md:max-w-5xl w-full ">
-      <div className={isOpen ? "bg-slate-black rounded-[20px] p-2 lg:p-4 text-white capitalize mx-auto" : "bg-slate-black rounded-full p-2 lg:p-4 text-white capitalize mx-auto"
-      }
+      <div
+        className={
+          isOpen
+            ? "bg-slate-950 rounded-[20px] p-2 lg:p-4 text-white capitalize mx-auto"
+            : "bg-slate-950 rounded-full p-2 lg:p-4 text-white capitalize mx-auto"
+        }
       >
         <nav className={isOpen ? "block" : "flex flex-wrap items-center"}>
-
-          {
-            isOpen ? (
-              <div className="flex justify-between mb-4">
-                <div className={"nav-brand"}>
-                  <Link to={"/"} className="flex items-center gap-2 text">
-                    <img src={brandLogo} alt="brand-logo" />
-                  </Link>
-                </div>
-
-                <div className="responsive_btnmd:hidden ml-auto">
-                  <button onClick={toggleResponsiveNav}>
-                    <img src={responsiveBtn} className="cursor-pointer" alt="responsive_btn" />
-                  </button>
-                </div>
+          {isOpen ? (
+            <div className="flex justify-between mb-4">
+              <div className={"nav-brand"}>
+                <Link to={"/"} className="flex items-center gap-2 text">
+                  <img src={brandLogo} alt="brand-logo" />
+                </Link>
               </div>
-            ) : (
-              <>
-                <div className={"nav-brand"}>
-                  <Link to={"/"} className="flex items-center gap-2 text">
-                    <img src={brandLogo} alt="brand-logo" />
-                  </Link>
-                </div>
 
-                <div className="responsive_btn md:hidden ml-auto">
-                  <button onClick={toggleResponsiveNav}>
-                    <img src={responsiveBtn} className="cursor-pointer" alt="responsive_btn" />
-                  </button>
-                </div>
-              </>
-            )
-          }
+              <div className="responsive_btnmd:hidden ml-auto">
+                <button onClick={toggleResponsiveNav}>
+                  <img
+                    src={responsiveBtn}
+                    className="cursor-pointer"
+                    alt="responsive_btn"
+                  />
+                </button>
+              </div>
+            </div>
+          ) : (
+            <>
+              <div className={"nav-brand"}>
+                <Link to={"/"} className="flex items-center gap-2 text">
+                  <img src={brandLogo} alt="brand-logo" />
+                </Link>
+              </div>
+
+              <div className="responsive_btn md:hidden ml-auto">
+                <button onClick={toggleResponsiveNav}>
+                  <img
+                    src={responsiveBtn}
+                    className="cursor-pointer"
+                    alt="responsive_btn"
+                  />
+                </button>
+              </div>
+            </>
+          )}
 
           <div className={isOpen ? "block" : "navbar mx-auto hidden md:block"}>
             <ul
@@ -102,26 +111,26 @@ const Header = () => {
                   "/forget-password" &&
                   "/reset-password" &&
                   "/add-priority") && (
-                  <Link
-                    to="/login"
-                    className="text-base font-bold capitalize border border-white py-3 px-8 rounded-full hover:bg-white hover:text-black transition-all duration-300"
-                  >
-                    Login
-                  </Link>
-                )}
+                <Link
+                  to="/login"
+                  className="text-base font-bold capitalize border border-white py-3 px-8 rounded-full hover:bg-white hover:text-black transition-all duration-300"
+                >
+                  Login
+                </Link>
+              )}
 
               {location.pathname !==
                 ("/signup" &&
                   "/verify-email" &&
                   "/successful-verification" &&
                   "/add-priority") && (
-                  <Link
-                    to="/signup"
-                    className="text-base font-bold capitalize border border-white py-3 px-8 rounded-full bg-white hover:bg-black text-black hover:text-white  transition-all duration-300"
-                  >
-                    Sign up
-                  </Link>
-                )}
+                <Link
+                  to="/signup"
+                  className="text-base font-bold capitalize border border-white py-3 px-8 rounded-full bg-white hover:bg-black text-black hover:text-white  transition-all duration-300"
+                >
+                  Sign up
+                </Link>
+              )}
               {location.pathname === "/add-priority" && (
                 <Link
                   to="#"
@@ -132,9 +141,7 @@ const Header = () => {
               )}
             </div>
           </div>
-
         </nav>
-
       </div>
     </div>
   );
